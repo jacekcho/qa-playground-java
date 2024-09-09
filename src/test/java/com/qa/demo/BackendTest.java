f6e9b66192839783d4452ab2e8bfed7aefe9e46e
Jacek Chowaniec <jacek.chowaniec@grandparade.co.uk>
2024-09-09 13:37:49 +0200

    init commit

---
+++ src/test/java/com/qa/demo/BackendTest.java
@@ -0,0 +1,11 @@
+package com.qa.demo;
+
+import org.testng.annotations.Test;
+
+public class BackendTest {
+
+    @Test
+    public static void shouldExecuteBackendTest() {
+        // TODO
+    }
+}
---
+++ src/test/java/com/qa/demo/FrontendTest.java
@@ -0,0 +1,11 @@
+package com.qa.demo;
+
+import org.testng.annotations.Test;
+
+public class FrontendTest {
+
+    @Test
+    public static void shouldExecuteFrontendTest() {
+        // TODO
+    }
+}
